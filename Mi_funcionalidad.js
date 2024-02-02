d3dcb0f9bd404e71c51f97cac97671f0e5077eaa
unknown <frandres9@hotmail.com>
2024-02-02 00:07:44 -0300

    cambios javascript

--- Mi_funcionalidad.js
+++ Mi_funcionalidad.js
@@ -1,1 +1,15 @@
-alert('hola')
+var contenedor = ['franco',
+{
+    puertas: 5,
+    color: 'blanco',
+    marca: 'chevrolet'
+}
+];
+
+console.log(contenedor[1].color)
+
+// if (usuario.edad > 28) {
+//     console.log(usuario.apellidos)
+// } else {
+//     console.log(usuario.nombre)
+// }
